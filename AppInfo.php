523b16ff25328cfbafb6acec3c80c709614b3cfd
gyaneshwar <gyaan1334@gmail.com>
2013-09-30 00:04:41 +0530

    Update AppInfo.php

--- AppInfo.php
+++ AppInfo.php
@@ -1,4 +1,4 @@
 <?php 
-define("FACEBOOK_APP_ID", '460203940697064');
-define("FACEBOOK_SECRET_KEY", '815d6af94c94c1b3fbbfa7f50bcd564a');
+define("FACEBOOK_APP_ID", 'xxxxxxxxxxxxxx');
+define("FACEBOOK_SECRET_KEY", 'xxxxxxxxxxxxxxxxxddddddd');
 ?>
